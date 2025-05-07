6ae9af0e11694036d60437961971933ccb66cc84
francis leon <francisleon1186@yahoo.com>
2025-05-07 10:23:48 -0500

    trabajando en analisis de usuarios

--- notebooks/analisisAsistencias.py
+++ notebooks/analisisAsistencias.py
@@ -10,37 +10,88 @@ dataFrameAsistencia=pd.read_csv("./data/asistencia_estudiantes_completo.csv")# .
 
 #print(dataFrameAsistencia.info())    # / informacion de los campos.desde int a objetos memory
 #print(dataFrameAsistencia.describe())#estadisticas
-#print(dataFrameAsistencia['estrato'].value_counts().head(2))#cada columna se vuelve un diccionario.
+print(dataFrameAsistencia['estrato'].value_counts().head(2))#cada columna se vuelve un diccionario.
 
 #ANTES DE FILTRAR COMO ANALISTA DE DATOS DEBES CONOCER (LA FUENTE PRIMARIA)
 #FILTROS Y CONDICIONES PARA TRANSFORMAR DATOS
-print(dataFrameAsistencia["estado"].unique())
+print(dataFrameAsistencia["estado"].unique())   
 print(dataFrameAsistencia["estrato"].unique())
 print(dataFrameAsistencia["medio_transporte"].unique())
+
 #1.REPORTAR TODOS LOS ESTUDIANTES QUE ASISTIERON 
 estudianteQueAsistieron=dataFrameAsistencia.query('estado=="asistio"')#aqui uso doble comillas porque estoy buscando un string 
 print(estudianteQueAsistieron)
 
 #2.REPORTAR TODOS LOS ESTUDIANTE FALTARON
+estudiantesQueFaltaron=dataFrameAsistencia.query('estado=="inasistencia"')
+print(estudiantesQueFaltaron)
 #3,REPORTAR TODOS LOS ESTUDIANTES QUE LLEGARON TARDE(JUSTIFICADO)
+estudiantesQueLLegaronTarde=dataFrameAsistencia.query('estado=="justificado"')
+print(estudiantesQueLLegaronTarde)
 #4.REPORTAR TODOS LOS ESTUDIANTES DE ESTRATO 1
 estudianteEstratoUno=dataFrameAsistencia.query('estrato==1')
 print(estudianteEstratoUno)
+
 #5.REPORTAR TODOS LOS ESTUDIANTESDE ESTRATO ALTO 
+print(dataFrameAsistencia['estrato'].value_counts())
+
 #6.REPORTAR TODOS LOS ESTUDIANTES QUE LLEGAN EN METRO
 estudiantesQueUsanMetro=dataFrameAsistencia.query('medio_transporte=="metro"')
 print(estudiantesQueUsanMetro)
-#7.REPORTAR TODOS LOS ESTUDIANTESQUE LLEGAN EN BICICLETA
+
+
+#7.REPORTAR TODOS LOS ESTUDIANTES QUE LLEGAN EN BICICLETA
+estudiantesBicicleta= dataFrameAsistencia.query('medio_transporte=="bicicleta"')
+print(estudiantesBicicleta)
+
+
+
 #8.REPORTAR TODOS LOS ESTUDIANTES QUE NO CAMINAN PARA LLEGAR A LA UNIVERSIDAD.
 estudiantesQueNoCaminan=dataFrameAsistencia.query('medio_transporte!="a pie"')
+
+
 #9.REPORTAR TODOS LOS REGISTROS DE ASISTENCIA DEL MES DE JUNIO.
+
+
+
+
+# Convert 'fecha' to datetime
+dataFrameAsistencia['fecha'] = pd.to_datetime(dataFrameAsistencia['fecha'], errors='coerce')
+
+# Filter: March dates and estado == 'asistio'
+march_asistio_june = dataFrameAsistencia[(dataFrameAsistencia['fecha'].dt.month == 3)&(dataFrameAsistencia['estado'] == 'asistio')]
+
+# Show result
+print(march_asistio_june)
+
+
+
 #10.REPORTAR TODOS LOS ESTUDIANTES QUE FALTARON Y USAN BUS PARA LLEGAR A LA UNIVERSIDAD.
 estuiantesQueFaltanyUsanBus=dataFrameAsistencia.query('medio_transporte=="bus" and estado=="inasistencia"')
 print(estuiantesQueFaltanyUsanBus)
 print(estuiantesQueFaltanyUsanBus.info())#info me dice exactamente cuantos estudiantes son.
+
+
+
 #11.REPORTAR TODOS LOS ESTUDIANTES QUE USAN BUS Y SON DE ESTRATOS ALTOS
+
+estudiantesQueEstratoAltoyUsanBus=dataFrameAsistencia[(dataFrameAsistencia['medio_transporte']=='bus')&(dataFrameAsistencia['estrato'].isin([4,5]))]
+
+print(estudiantesQueEstratoAltoyUsanBus)
+count=len(estudiantesQueEstratoAltoyUsanBus)
+print(f'total of {count} que usan bus y son estratos altos')
+
+
 #12.REPORTAR TODOS LOS ESTUDIANTES QUE USAN BUS Y SON DE ESTRATOS BAJOS
+
+estudiantesEstratosBajosyUsanBus=dataFrameAsistencia[(dataFrameAsistencia['medio_transporte']=='bus')&(dataFrameAsistencia['estrato'].isin([1,2]))]
+count1=len(estudiantesEstratosBajosyUsanBus)
+print(estudiantesEstratosBajosyUsanBus)
+print(f'total of {count1} que usan bus y son estratos bajos')
+'''
 #13.REPORTAR TODOS LOS ESTUDIANTES QUE LLEGAN TARDE Y SON DE ESTRATO 3,4,5,O 6
+
+
 #14.REPORTAR TODOS LOS ESTUDIANTES QUE USAN TRANSPORTES ECOLOGICOS
 #15.REPORTAR TODOS LOS ESTUDIANTES QUE FALTAN Y USAN CAROO PARA TRANSPORTARSE
 #16. REPORTAR TODOS LOS ESTUDIANTES QUE ASISTEN SON ESTRATOS ALTOS Y CAMINAN
@@ -76,3 +127,9 @@ print(promedioEstratoPorEstado)
 #13 PROMEDIO DE ESTRATO POR FECHA.
 #14.NUMERO DE TIPOS DE ESTADO POR TRANSPORTE
 #15.PRIMER REGSITRO DE CADA GRUPO 
+'''
+
+
+print(dataFrameAsistencia["estado"].unique())
+print(dataFrameAsistencia["estrato"].unique())
+print(dataFrameAsistencia["medio_transporte"].unique())
--- notebooks/analisisUsuarios.py
+++ notebooks/analisisUsuarios.py
@@ -1,6 +1,115 @@
 import pandas as pd
 
 dataFrameUsuarios=pd.read_excel("./data/usuarios_sistema_completo.xlsx")#ojo que si no te lee. borrar el primer punto.
+'''
 print(dataFrameUsuarios)
 
 
+
+print(dataFrameUsuarios["tipo_usuario"].unique())
+
+print(dataFrameUsuarios["especialidad"].unique())
+print(dataFrameUsuarios.info())
+
+
+print(dataFrameUsuarios["especialidad"].unique())
+
+
+
+#1. Solo estudiantes
+soloEstudiantes=dataFrameUsuarios.query('tipo_usuario=="estudiante"')
+print(soloEstudiantes)
+
+#2. Solo profesores
+
+soloDocentes=dataFrameUsuarios.query('tipo_usuario=="docente"')
+print(soloDocentes)
+#3. Todos excepto estudiantes
+
+
+#4. Filtrar por especialidad
+
+filtroEspecialidad=dataFrameUsuarios[dataFrameUsuarios['especialidad'].notna()]
+print(filtroEspecialidad)
+#5. Excluir una especialidad
+filtroPorEspecialidad=dataFrameUsuarios[dataFrameUsuarios['especialidad']=="Ingenieria de Sistemas"]
+print(filtroPorEspecialidad)
+#6. Excluir administrativos
+soloAdministrative=dataFrameUsuarios.query('tipo_usuario=="administrativos"')
+print(soloAdministrative)
+
+#7. Direcciones en medellin
+df_Medellin=dataFrameUsuarios[dataFrameUsuarios['direccion'].str.lower().str.contains('medellin',na=False)]#.contains('medellin',na=False)
+print(df_Medellin)
+
+#8. Direcciones terminadas en sur
+#df_sur = dataFrameUsuarios[dataFrameUsuarios['direccion'].fillna('').str.lower().str.endswith('sur')]
+#rint(df_sur)
+
+#9. Direcciones que inician con calle
+df_calle=dataFrameUsuarios[dataFrameUsuarios['direccion'].fillna('').str.lower().str.startswith('calle')]
+print(df_calle)
+
+#10.Especialidades que contienen la palabra datos
+df_ContieneDatos=dataFrameUsuarios[dataFrameUsuarios['especialidad'].str.lower().str.contains('dato',na=False)]#.contains('medellin',na=False)
+print(df_ContieneDatos)
+
+#11. instructores en itagui
+
+df_filtro = dataFrameUsuarios[
+    dataFrameUsuarios['especialidad'].notna()  # Filtra direcciones que terminan en 'sur'
+    & dataFrameUsuarios['direccion'].str.contains('itagui', case=False, na=False)  # Filtra direcciones que contienen 'Medellín'
+]
+print(df_filtro)  
+
+'''
+#12. nacidos despues de 2000
+
+dataFrameUsuarios['fecha_nacimiento'] = pd.to_datetime(dataFrameUsuarios['fecha_nacimiento'])
+
+# Filtrar personas nacidas después de 2000
+df_nacidos_despues_2000 = dataFrameUsuarios[(dataFrameUsuarios['fecha_nacimiento'] > '2000-01-01')]
+print(df_nacidos_despues_2000)
+
+
+#13. nacidos en los 90
+
+dataFrameUsuarios['fecha_nacimiento']=pd.to_datetime(dataFrameUsuarios['fecha_nacimiento'])
+df_nacidos90=dataFrameUsuarios[dataFrameUsuarios['fecha_nacimiento'].dt.year==1990]
+print(df_nacidos90)
+
+#14. direcciones en envigado
+#15. especialdiades que empizan por I
+#16. usuarios sin direccion
+#17. usuarios sin especialidad
+#18. profesores que viven en sabaneta
+#19. aprendices que viven en bello
+#20. nacidos en el nuevo milenio
+
+
+#1. total por tipo
+#2. total por especialidad
+#3. cantidad de especialidades distintas
+#4. tipos de usuario por especialidad
+#5. usuario mas antiguo por tipo
+#6. usuario mas joven por tipo
+#7. primer registro por tipo
+#8. ultimo registro por tipo
+#9. combinacion tipo por especialidad
+#10. el mas viejo por especialidad
+#11. cuantos de cada especialidad por tipo
+#12. edad promedio por tipo
+#13. años de nacimeinto mas frecuente por especialidad
+#14. mes de nacimiento ams frecuente por tipo
+#15. UNA CONSULTA O FILTRO QUE UD PROPONGA
+
+
+
+
+#print(dataFrameUsuarios["tipo_usuario"].unique())
+
+#print(dataFrameUsuarios["especialidad"].unique())
+#print(dataFrameUsuarios["direccion"].unique())
+#print(dataFrameUsuarios.info())
+
+#print(dataFrameUsuarios)
